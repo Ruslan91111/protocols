ee702bc3106f420facf2f7db557f5a3f70e9a79c
Минуллин Руслан Ильдусович <RIMinullin@dixy.local>
2024-04-12 19:02:03 +0300

    first commit convert_files_in_dir.py - module to convert all PDF files in dir to WORD files

---
+++ convert_files_in_dir.py
@@ -0,0 +1,250 @@
+"""
+Конвертировать все файлы формата PDF из одной папки в WORD файлы в другую папку.
+"""
+import os.path
+import time
+from typing import Optional
+from enum import Enum
+import psutil
+import py_win_keyboard_layout
+import pyautogui
+import pyperclip
+
+from exceptions import ScreenshotNotFoundException
+
+
+DIR_SCREENSHOTS = r'./screenshots/'
+DIR_WITH_PDF_FILES = r'C:\Users\RIMinullin\Documents\протоколы'
+DIR_FOR_WORD_FILES = r'C:\Users\RIMinullin\Desktop\для ворда'
+
+FINE_READER_PROCESS = 'FineReader.exe'
+WORD_PROCESS = 'WINWORD.EXE'
+
+MESSAGE_FOR_USER_GET_DIR_WITH_PDF_FILES = (
+    'Введите полный путь (включая сетевой диск) до директории, '
+    'в которой находятся файлы формата PDF, нуждающиеся в конвертации в формат Word:'
+    '\n>>>')
+
+MESSAGE_FOR_USER_GET_DIR_WITH_WORD_FILES = (
+    'Введите полный путь (включая сетевой диск) до директории, '
+    'в которой будут находиться файлы Word после конвертации:'
+    '\n>>>')
+
+
+class FineReaderScreenshots(Enum):
+    """
+    Пути к скриншотам, необходимым для работы с FineReader с помощью pyautogui.
+    """
+    FINE_READER_DESKTOP: str = r'./screenshots/fine_reader_desktop.png'
+    FINE_READER_PANEL: str = r'./screenshots/fine_reader_panel.png'
+    FINE_READER_LOADING: str = r'./screenshots/fine_reader_loading.png'
+    BUTTON_CONVERT_TO_WORD_MAIN_MENU: str = r'./screenshots/button_convert_to_word_main_menu.png'
+    BLUE_BUTTON_CONVERT_TO_WORD: str = r'./screenshots/blue_button_convert_to_word.png'
+    DATA_OF_MODIFICATION: str = r'./screenshots/data_of_modification.png'
+    INPUT_FILE_NAME: str = r'./screenshots/input_file_name.png'
+    PROCESS_OF_CONVERSION: str = r'./screenshots/process_of_conversion.png'
+    BUTTON_CANCEL: str = r'./screenshots/button_cancel.png'
+    SAVE_BUTTON: str = r'./screenshots/save_button.png'
+
+
+##################################################################################
+# Функции работы со скриншотами.
+##################################################################################
+def wait_and_click_screenshot(screenshot_path: str,
+                              timeout: int = 5,
+                              confidence: float = 0.8) -> tuple[float] | None:
+    """
+    Ожидать появление скриншота в течение заданного времени.
+     При обнаружении кликнуть один раз.
+
+    :param screenshot_path: путь к скриншоту,
+    :param timeout: время, в течении которого мы ждем скриншот
+    :param confidence: уровень уверенности в том, что это именно тот скриншот
+    :return: координаты найденного скриншота.
+
+    """
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        try:
+            time.sleep(0.03)
+            image = pyautogui.locateOnScreen(screenshot_path, confidence=confidence)
+            # Если скриншот найден кликнуть по нему и вернуть его координаты
+            if image:
+                pyautogui.click(image)
+                return image
+        except pyautogui.ImageNotFoundException:
+            pass
+    # Поднять исключение, если скриншот не найден в течении заданного периода времени.
+    raise ScreenshotNotFoundException(screenshot_path)
+
+
+def waiting_disappear_screenshot(screenshot: str,
+                                 timeout: int = 10,
+                                 confidence: float = 0.8) -> Optional[False]:
+    """
+    Ждем пока определенный скриншот не исчезнет с экрана.
+
+    В течении заданного периода времени ищем скриншот на экране. Если находим,
+    то продолжаем искать, если не находим, то выходим из функции и возвращаем None.
+    Если в течении заданного времени скриншот с экрана не ушел, то возвращаем False.
+
+    :param screenshot:
+    :param timeout:
+    :param confidence:
+    :return: если скриншот исчез с экрана, то выходим из функции, возвращаем None
+    """
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        try:
+            time.sleep(0.05)
+            # Ищем скриншот.
+            image = pyautogui.locateOnScreen(screenshot, confidence=confidence)
+            # Если найден, то продолжаем его находить.
+            if image:
+                pass
+        except pyautogui.ImageNotFoundException:
+            return None
+    return False
+
+
+##################################################################################
+# Функции, связанные с работой с процессами, папками и файлами.
+##################################################################################
+def launch_desktop_app(process: str,
+                       icon_from_panel: str,
+                       icon_from_desktop: str,
+                       confidence: float = 0.8,
+                       ) -> None:
+    """
+    Проверяет запущен ли переданный процесс в ОС. Если процесс(окно) запущено, то
+    раскрываем кликом по иконке на панели. Если нет, то кликаем по иконке на рабочем столе.
+
+    :param process: Процесс, который проверяем запущен он или нет.
+    :param icon_from_panel:
+    :param icon_from_desktop:
+    :param confidence:
+    :return: None
+
+    """
+    # Переключить раскладку
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(0x04090409)
+    # Свернуть все окна
+    pyautogui.hotkey('Win', 'd')
+    # Перебираем текущие процессы и ищем нужный нам процесс.
+    for proc in psutil.process_iter():
+        name = proc.name()
+        # Если нужный процесс запущен.
+        if name == process:
+            # Кликнуть по иконке на нижней панели, чтобы развернуть окно.
+            wait_and_click_screenshot(icon_from_panel, confidence=confidence)
+            return None
+    # Если нужный процесс не запущен, то запускаем по иконке с рабочего стола.
+    wait_and_click_screenshot(icon_from_desktop, confidence=confidence)
+    pyautogui.hotkey('enter')
+    return None
+
+
+def terminate_the_proc(process: str) -> None:
+    """
+    Закрыть переданный процесс.
+
+    :param process: Процесс, который проверяем запущен он или нет.
+    :return: None
+    """
+    # Перебираем текущие процессы и ищем нужный нам процесс.
+    for proc in psutil.process_iter():
+        name = proc.name()
+        # Если нужный процесс запущен.
+        if name == process:
+            proc.terminate()
+
+
+def get_path_for_existing_dir(message_for_user: str) -> str:
+    """Получить путь от пользователя к существующей директории."""
+    path_from_user = input(message_for_user)
+
+    # Если путь указан неверно.
+    if not path_from_user:
+        raise Exception("Путь не введен")
+    # Если файл не существует.
+    if not os.path.isdir(path_from_user):
+        raise Exception("Указанный путь не существует")
+    return path_from_user
+
+
+def get_files_required_to_convert(path_to_dir_pdf: str, path_to_dir_word: str) -> set:
+    """Вернуть множеством файлы, которые нужно конвертировать."""
+    pdf_files = {i for i in os.listdir(path_to_dir_pdf) if i[-4:] == '.pdf'}
+    pdf_files = {i[:-4] for i in pdf_files}
+    word_files = {i for i in os.listdir(path_to_dir_word) if i[-5:] == '.docx'}
+    word_files = {i[:-5] for i in word_files}
+    set_files_required_to_convert = pdf_files.difference(word_files)
+    return set_files_required_to_convert
+
+
+##################################################################################
+# Главная функция модуля.
+##################################################################################
+def convert_all_pdf_in_dir_to_word() -> None:
+    """
+    Получить от пользователя путь к директории с PDF файлами и путь к директории,
+    в которую будут сохранены файлы в формате word после конвертации.
+
+    :return:
+    """
+    # Получить от пользователя пути к: 1) директория с пдф файлами и 2) word файлами
+    dir_with_pdf_files = get_path_for_existing_dir(MESSAGE_FOR_USER_GET_DIR_WITH_PDF_FILES)
+    dir_for_word_files = get_path_for_existing_dir(MESSAGE_FOR_USER_GET_DIR_WITH_WORD_FILES)
+    # Перечень файлов, которые еще не конвертированы.
+    files_required_to_convert = get_files_required_to_convert(dir_with_pdf_files,
+                                                              dir_for_word_files)
+    # Запустить FineReader.
+    launch_desktop_app(FINE_READER_PROCESS,
+                       FineReaderScreenshots.FINE_READER_PANEL.value,
+                       FineReaderScreenshots.FINE_READER_DESKTOP.value,
+                       0.8)
+    # Дождаться полной загрузки FineReader.
+    waiting_disappear_screenshot(FineReaderScreenshots.FINE_READER_LOADING.value)
+
+    # Перебираем файлы в перечне файлов, нуждающихся в конвертации.
+    for file in files_required_to_convert:
+        # Главное меню, кликнуть по кнопке конвертировать.
+        wait_and_click_screenshot(FineReaderScreenshots.BUTTON_CONVERT_TO_WORD_MAIN_MENU.value)
+        # Ввести путь и наименование файла PDF, который нужно конвертировать.
+        wait_and_click_screenshot(FineReaderScreenshots.INPUT_FILE_NAME.value)
+        x, y = pyautogui.position()
+        pyautogui.click(x + 80, y)
+        pyautogui.hotkey('ctrl', 'a')
+        pyautogui.press('backspace')
+        filename_pdf = dir_with_pdf_files + '\\' + file
+        pyperclip.copy(filename_pdf.replace('\\\\', '\\'))
+        pyautogui.hotkey('ctrl', 'v')
+        pyautogui.press('enter')
+
+        # Нажать синюю кнопку конвертировать в Word.
+        wait_and_click_screenshot(FineReaderScreenshots.BLUE_BUTTON_CONVERT_TO_WORD.value)
+
+        # Ввести путь и наименование word файла, в котором будет сохранен результат конвертации.
+        wait_and_click_screenshot(FineReaderScreenshots.INPUT_FILE_NAME.value)
+        x, y = pyautogui.position()
+        pyautogui.click(x + 80, y)
+        pyautogui.hotkey('ctrl', 'a')
+        pyautogui.press('backspace')
+        filename_pdf = dir_for_word_files + '\\' + file
+        pyperclip.copy(filename_pdf.replace('\\\\', '\\'))
+        pyautogui.hotkey('ctrl', 'v')
+        pyautogui.press('enter')
+
+        # Нажать кнопку сохранить.
+        wait_and_click_screenshot(FineReaderScreenshots.SAVE_BUTTON.value)
+
+        # Дождаться окончания конвертации.
+        waiting_disappear_screenshot(FineReaderScreenshots.PROCESS_OF_CONVERSION.value, timeout=40)
+
+        # Закрыть word файл, который открывается автоматически после конвертации.
+        terminate_the_proc(WORD_PROCESS)
+        wait_and_click_screenshot(FineReaderScreenshots.BUTTON_CANCEL.value)
+
+
+if __name__ == '__main__':
+    convert_all_pdf_in_dir_to_word()
